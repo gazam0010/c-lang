89424defa3b1c919bb3bfa192fe00a392a7c3e1f
gazam0010 <71376010+gazam0010@users.noreply.github.com>
2022-02-25 22:00:00 +0530

    Create diamond-struct.c

---
+++ diamond-struct.c
@@ -0,0 +1,33 @@
+#include <stdio.h> 
+int main()       
+{
+    int n, i, j, k;
+    printf("Enter number of rows to make diamond structure: ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n - i; j++)
+        {
+            printf(" ");
+        }
+        for (k = 1; k <= i; k++)
+        {
+            printf("%d ", k);
+        }
+        printf("\n");
+    }
+
+    for (i = 1; i <= n - 1; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf(" ");
+        }
+        for (k = 1; k <= n - i; k++)
+        {
+            printf("%d ", k);
+        }
+        printf("\n");
+    }
+    return 0;
+}
